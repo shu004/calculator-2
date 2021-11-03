09a48c2465ecf68f0270cf2a93d4feb75f6e5e59
Sisi Hu <shu004@gmail.com>
2021-11-03 17:11:36 -0600

    IRead and tokenize input, add function

--- calculator.py
+++ calculator.py
@@ -5,3 +5,18 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+
+#get user input save that to a variable
+#split by spaces to a list
+
+while True:
+    command = input("> ").split(" ")
+
+    if command[0] == "q":
+        break
+    else:
+        num1 = float(command[1])
+        num2 = float(command[2])
+        if command[0] == '+':
+            print(add(num1, num2))
+
